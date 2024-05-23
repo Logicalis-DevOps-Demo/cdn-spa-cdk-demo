6ff34fd85118adf63d350ba563b7c2c0fcbbb125
Alberto González <alberto.gonzalez@es.logicalis.com>
2024-05-23 18:45:49 +0200

    0.1.20

--- software/angular/src/app/dynamodb-crud/dynamodb-crud.component.spec.ts
+++ software/angular/src/app/dynamodb-crud/dynamodb-crud.component.spec.ts
@@ -27,11 +27,36 @@ describe('DynamodbCRUDComponent', () => {
   });
 
 
-  it('should render title', () => {
+  it('put User', () => {
     const fixture = TestBed.createComponent(DynamodbCRUDComponent);
     component = fixture.componentInstance;
     component.putUser()
     expect(component.resultadoObject?.message).toBeUndefined();
   });
+
+
+  it('putUserCompleteResponse', () => {
+    const fixture = TestBed.createComponent(DynamodbCRUDComponent);
+    component = fixture.componentInstance;
+    component.putUserCompleteResponse()
+    expect(component.resultadoObject?.message).toBeUndefined();
+  });
+
+
+  it('getUserCompleteResponse', () => {
+    const fixture = TestBed.createComponent(DynamodbCRUDComponent);
+    component = fixture.componentInstance;
+    component.getUserCompleteResponse()
+    expect(component.resultadoObject?.message).toBeUndefined();
+  });
+
+
+  it('deleteUserCompleteResponse', () => {
+    const fixture = TestBed.createComponent(DynamodbCRUDComponent);
+    component = fixture.componentInstance;
+    component.deleteUserCompleteResponse()
+    expect(component.resultadoObject?.message).toBeUndefined();
+  });
+
 
 });
